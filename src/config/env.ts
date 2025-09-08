b099b4b4112fe6118139a6ad620deb1b596af7ac
Shaurya55555 <bajpaishaurya2911@gmail.com>
2025-09-08 18:28:42 +0530

    final commit

---
+++ src/config/env.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const config = {
+  PORT: process.env.PORT || 37849,
+  NODE_ENV: process.env.NODE_ENV || "development",
+};
+ 
---
+++ src/routes/app.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { getStatus } from "../controllers/app.controller";
+
+const router = Router();
+router.get("/status", getStatus);
+
+export default router;
+ 
